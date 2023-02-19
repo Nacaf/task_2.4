70597a372641814b60c0ff51766e8e5b9f5ce75b
Najaf Sharifov <nwerifov@mail.ru>
2023-02-19 21:41:04 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//Task 2.4
+
+/*let num;
+let str = String(num);*/
+
+/*let price = prompt("Enter price in format 'mmm.qq'");
+let mainPrice = price.split(".");
+console.log(mainPrice[0] + " manat " + mainPrice[1] + " qəpik");*/
+
+/*let grossSalary = prompt("Enter your salary");
+let netSalary = +grossSalary - (+grossSalary*0.13);
+console.log(netSalary);*/
+
+/*let fullName = prompt("Enter full name"); 
+let user = fullName.split(" ");
+console.log(user[0], user[1][0] + "." + user[2][0] + ".");*/
+
+//Task 2.4 Bonus
+
+/*let depositAmount = prompt("Enter your deposit amount");
+let numberOfMonth = prompt("Enter number of month");
+let perccentageOfYear = prompt("Enter percentage of the year");
+let profitFromDeposit = (depositAmount*perccentageOfYear)/100;
+alert(`"${numberOfMonth} aydan sonra əmanətdən qazanciniz ${profitFromDeposit} olacaq"`);*/
